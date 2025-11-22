618e9e2fb760eb2e01cc513a104b8c2b900daee7
Yerko Flores <yerkonicolas.flores@alumnos.ulagos.cl>
2025-11-22 20:37:04 -0300

    ajustando parametros

--- ajustes.py
+++ ajustes.py
@@ -1,9 +1,10 @@
-WIDTH = 640
-HEIGHT = 480
-TILE = 32
+WIDTH = 800
+HEIGHT = 600
+TILE = 25 #medida de un bloque 25*25
 
 FPS = 60
 
+# Colores
 MORADO = (126, 0, 176) #fondo
 BLANCO = (255, 255, 255) #BLANCO, color laberinto
 AZUL = (50, 100, 255)
