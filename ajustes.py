2a6fd79ec9ec9c555e7b420f5f999cae163af185
yerko flores <yerkonicolas.flores@alumnos.ulagos.cl>
2025-11-26 12:47:13 -0300

    modificando laberinto

--- ajustes.py
+++ ajustes.py
@@ -5,6 +5,6 @@ TILE = 25 #medida de un bloque 25*25
 FPS = 60
 
 # Colores
-MORADO = (126, 0, 176) #fondo
-BLANCO = (255, 255, 255) #BLANCO, color laberinto
-AZUL = (50, 100, 255)
+MORADO = (79, 12, 107) #paredes del laberinto
+BLANCO = (0,0,0) #Fondo
+AZUL = (255, 230, 0) #amarillo
