1344ca8270915035ceef5524c9e6825867041610
Marco Saldivia <marcoantonio.saldivia@alumnos.ulagos.cl>
2025-12-15 22:16:05 -0300

    Ajustes en el comportamiento del enemigo

--- enemy.py
+++ enemy.py
@@ -1,35 +1,49 @@
 import pygame
-from ajustes import TILE, ROJO
+from ajustes import TILE
 
 class Enemy():
-    def __init__(self, x, y):
-        #posicion inicial
+    def __init__(self, x, y, movement="vertical", speed=2):
+        # posicion inicial
         self.start_x = x
         self.start_y = y
 
-        #cargar sprite
-        self.image = pygame.image.load('sprites/fantasma.png').convert_alpha()
-        
-        #redimensionar el tamaño
-        self.image = pygame.transform.scale(self.image, (int(TILE*0.9), int(TILE*0.9)))
+        # tipo de movimiento: "vertical" o "horizontal"
+        self.movement = movement
 
-        #rectangulo basado en la imagen
+        # velocidad
+        self.speed = speed
+
+        # rango de movimiento
+        self.move_range = 4 * TILE
+        self.direction = 1
+
+        # cargar sprite
+        self.image = pygame.image.load('sprites/fantasma.png').convert_alpha()
+        self.image = pygame.transform.scale(
+            self.image, (int(TILE * 0.9), int(TILE * 0.9))
+        )
+
+        # rectangulo del enemigo
         self.rect = self.image.get_rect(topleft=(x, y))
 
-        #movimiento vertical
-        self.speed = 2
-        self.move_range = 3 * TILE #3 espacios hacia arriba y abajo
-        self.direction = 1 #1 = abajo, -1 = arriba
-
     def update(self):
-        #mover en Y (vertical)
-        self.rect.y += self.direction * self.speed
+        # movimiento vertical
+        if self.movement == "vertical":
+            self.rect.y += self.direction * self.speed
 
-        #cambiar direccion cuando llega al limite
-        if self.rect.y > self.start_y + self.move_range:
-            self.direction = -1
-        if self.rect.y < self.start_y:
-            self.direction = 1
+            if self.rect.y > self.start_y + self.move_range:
+                self.direction = -1
+            if self.rect.y < self.start_y:
+                self.direction = 1
+
+        # movimiento horizontal
+        elif self.movement == "horizontal":
+            self.rect.x += self.direction * self.speed
+
+            if self.rect.x > self.start_x + self.move_range:
+                self.direction = -1
+            if self.rect.x < self.start_x:
+                self.direction = 1
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
